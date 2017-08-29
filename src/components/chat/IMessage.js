25aa91fa8bd13be9295a3885ea21d9a70fd48245
huangjun <hj644473762@gmail.com>
2017-08-29 16:00:24 +0800

    v2

--- src/components/chat/IMessage.js
+++
@@ -1,6 +0,0 @@
-
-export const MESSAGE_FLAG_ACK = 2;
-export const MESSAGE_FLAG_FAILURE = 8;
-export const MESSAGE_FLAG_UPLOADING = 16;
-export const MESSAGE_FLAG_SENDING = 32;
-export const MESSAGE_FLAG_LISTENED = 64;
--- src/components/chat/Themes/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import Colors from './Colors'
-import Fonts from './Fonts'
-import Metrics from './Metrics'
-import Images from './Images'
-
-export { Colors, Fonts, Images, Metrics }
